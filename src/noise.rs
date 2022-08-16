323d18e898cf0a937ef05c719d717aa30b824926
Dark Kitsu <cbmuench@hotmail.com>
2022-08-16 09:52:59 -0700

    Commentary

--- src/noise.rs
+++ src/noise.rs
@@ -23,15 +23,21 @@ impl<const DIMENSIONS: usize> Noise<DIMENSIONS> {
     }
 
     fn _raw_noise(&self, position: Vector<i64, DIMENSIONS>) -> f64 {
+        // Combine the seed and the position into a single seed and generate a random floating point value from 0 to 1.
         (self.seed, position).into_random::<f64>()
     }
 
     fn _raw_noise_smooth(&self, position: Vector<f64, DIMENSIONS>) -> f64 {
+        // Find the corner of the "cell" which the position lays inside
         let min_corner = Vector::new([init_array!([i64; DIMENSIONS], |idx| position
             .component(idx)
             .unwrap()
             .floor()
             as i64)]);
+        // Generate an interpolated value by using how close the position is to each corner of the cell,
+        // and interpolating between the noise samples at said corners.
+        // i.e. if the position is in the center of the cell, the final sample will be the average of of the noise samples
+        // at all 4 corners
         match DIMENSIONS {
             // 1 dimensional noise
             1 => self._raw_noise(min_corner).lerp(
@@ -86,15 +92,24 @@ impl<const DIMENSIONS: usize> Noise<DIMENSIONS> {
     }
 
     pub fn sample_f64(&self, mut position: Vector<f64, DIMENSIONS>) -> f64 {
+        // Scale the position by the scale factor an add a random offset to prevent artifacts from noise patterns
         position = position / self.scale + self.seed.into_random::<f64>() * 111098765.4321;
+        // Generate the noise value at the position for level 0, weighted by the detail strength at level 0 (self.detail_strength)
         let mut sum = self._raw_noise_smooth(position) * self.detail_strength;
+        // total_strength will hold the summed strength
         let mut total_strength = self.detail_strength;
+        // For each level beyond 0:
         for idx in 1..self.levels {
+            // Divide the position by the level's scale factor which is calculated from self.smoothness, plus a random offset
             position = position / (1.0 + self.smoothness) + (idx as u64, self.seed).into_random::<f64>() * 111098765.4321;
+            // Calculate the strength for this level
             let this_strength = self.detail_strength.lerp(1.0, idx as f64 / self.levels as f64);
+            // Add the sample weighted by the level's strength to the total sum
             sum += self._raw_noise_smooth(position) * this_strength;
+            // Add the level's strength to the total strength
             total_strength += this_strength;
         }
+        // Final sample is the sum of every level's weighted sample, divided by the total strength of every level
         sum / total_strength
     }
 }
